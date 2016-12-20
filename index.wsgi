29231f35c85291df62e2078b2289778b012494b4
gp163_zhao@163.com <gp163_zhao@163.com>
2016-12-20 23:58:26 +0800

    做微信请求验证，用 flask

--- index.wsgi
+++ index.wsgi
@@ -1,9 +1,4 @@
 import sae
-
-def app(environ, start_response):
-    status = '200 OK'
-    response_headers = [('Content-type', 'text/plain')]
-    start_response(status, response_headers)
-    return ['Hello, world!']
+from newedu import app
 
 application = sae.create_wsgi_app(app)
---
+++ newedu.py
@@ -0,0 +1,23 @@
+import hashlib
+
+__author__ = 'zhaoguoping'
+from flask import Flask, request, make_response
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/', methods=['GET', 'POST'])
+def wechat_auth():
+    if  request.method == 'GET':
+        token = 'sae'
+        query = request.args
+        signature = query.get('signature', )
+        timestamp = query.get('timestamp', )
+        nonce = query.get('nonce', )
+        echostr = query.get('echostr', )
+        s = [token, timestamp, nonce]
+        s.sort()
+        s = ''.join(s)
+        if hashlib.sha1(s).hexdigest() == signature:
+            return make_response(echostr)
+
