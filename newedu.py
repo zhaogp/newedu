c341aa3daeeb133b3ee073d769c3225ac17fa2d5
gp163_zhao@163.com <gp163_zhao@163.com>
2016-12-24 13:43:51 +0800

    微信开发，输入什么输出什么

--- newedu.py
+++ newedu.py
@@ -1,4 +1,6 @@
 import hashlib
+import xml.etree.ElementTree as ET
+import time
 
 __author__ = 'zhaoguoping'
 from flask import Flask, request, make_response
@@ -21,9 +23,24 @@ def wechat_auth():
         s.sort()
         s = ''.join(s)
         if hashlib.sha1(s).hexdigest() == signature:
-            response = make_response(echostr)
+            one_response = make_response(echostr)
         else:
-            response = make_response('auth error ...')
-    response.content_type = 'application/text'
+            one_response = make_response('auth error ...')
+    one_response.content_type = 'application/text'
+
+    xml_recv = ET.fromstring(request.data)
+    to_user_name = xml_recv.find('ToUserName').text
+    from_user_name = xml_recv.find('FromUserName').test
+    content = xml_recv.find('Content').text
+    reply = '<xml>' \
+            '<ToUserName><![CDATA[%s]]></ToUserName>' \
+            '<FromUserName><![CDATA[%s]]></FromUserName>' \
+            '<CreateTime>%s</CreateTime>' \
+            '<MsgType><![CDATA[text]]></MsgType>' \
+            '<Content><![CDATA[%s]]></Content>' \
+            '<FuncFlag>0</FuncFlag>' \
+            '</xml>'
+    response = make_response(reply % (to_user_name, from_user_name, str(int(time.time())), content))
+    response.content_type = 'application/xml'
     return response
 
